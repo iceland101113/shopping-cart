1a0388b6f0540a64cf2eb5f060679b7a4895b427
iceland101113 <r02521115@ntu.edu.tw>
2018-04-09 23:31:42 +0800

    add product new and create actions and views for admin

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -6,4 +6,25 @@ class Admin::ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "product was successfully created"
+      redirect_to admin_products_path
+    else
+      flash.now[:alert] = "product was failed to create"
+      render :new
+    end
+  end
+
+private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price)
+  end
+
 end
