65880d6a94909e0ce32c2cd419d42a37848f3055
iceland101113 <r02521115@ntu.edu.tw>
2018-04-10 21:56:44 +0800

    add product  edit and update actions and views for admin

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,7 @@
 class Admin::ProductsController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_admin
+  before_action :set_product, only: [:show, :edit, :update]
 
   def index
     @products = Product.all
@@ -22,10 +23,26 @@ class Admin::ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
+  end
+
+  def edit 
+  end
+
+  def update
+    if @product.update(product_params)
+      flash[:notice] = "product was successfully updated"
+      redirect_to admin_product_path(@product)
+    else
+      flash.now[:alert] = "product was failed to update"
+      render :edit
+    end
   end
 
 private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
   def product_params
     params.require(:product).permit(:name, :description, :price)
