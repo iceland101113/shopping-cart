6ebb1c49fe0b08a3f1fac3b786c516d658a57337
iceland101113 <r02521115@ntu.edu.tw>
2018-04-15 19:06:51 +0800

    show total cart item count

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
   def index
     @products = Product.page(params[:page]).per(10)
     @cart = Cart.create(number: "1")
-    
+    @cart_product = Product.new
   end
 
   def show
@@ -15,8 +15,8 @@ class ProductsController < ApplicationController
     @cart_product = Product.find(@product_id)
     @cart_id = Cart.last.id
     @cartitem = Cartitem.create(product_id: @product_id, cart_id: @cart_id)
-
-    render :json => { :product_id => @product_id, :cart_id=> @cart_id, :cartitem_id=> @cartitem.id}
+    @cart_tol = Cartitem.where(cart_id: @cart_id).count
+    render :json => { :product_id => @product_id, :cart_id=> @cart_id, :cartitem_id=> @cartitem.id, :cart_product_name=> @cart_product.name, :cart_product_price=> @cart_product.price, :cart_product_image=> @cart_product.image, :cart_tol=> @cart_tol}
   end
 
 private
